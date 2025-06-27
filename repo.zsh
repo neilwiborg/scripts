7e7af996d0df1c6f3fd8c4334e0d780edd393462
Neil Wiborg <NeilWiborg@outlook.com>
2025-06-26 21:18:03 -0700

    support multiple sources dirs

--- repo.zsh
+++ repo.zsh
@@ -1,6 +1,6 @@
 #!/usr/bin/env zsh
 
-typeset -g sources_dir="$HOME/cs"
+typeset -ga sources_dirs=("$HOME/cs")
 
 repo() {
 	# if no args or too many args passed
@@ -11,8 +11,15 @@ repo() {
 
 	# convert query into lowercase
 	local search_term=$(echo "$1" | tr '[:upper:]' '[:lower:]')
-	# search for directories and symlinks only one level deep in $sources_dir, and search case-insensitively
-	local target=$(find "$sources_dir" -maxdepth 1 \( -type d -o -type l \) -iname "*$search_term*" | head -n 1)
+	local target=""
+
+	# loop over all sources directories
+	for dir in "${sources_dirs[@]}"; do
+		# search for directories and symlinks only one level deep in $sources_dir, and search case-insensitively
+		target=$(find "$dir" -maxdepth 1 \( -type d -o -type l \) -iname "*$search_term*" | head -n 1)
+		# if we find the target, then break
+		[[ -n "$target" ]] && break
+	done
 
 	# if target is not empty
 	if [ -n "$target" ]; then
@@ -25,8 +32,13 @@ repo() {
 
 # Tab completion for repo function
 _repo_complete() {
-	# get just the basenames of all directories and symlinks and remove the first line ($sources_dir)
-	local repos=($(find "$sources_dir" -maxdepth 1 \( -type d -o -type l \) -exec basename {} \; | sed '1d'))
+	local -a repos
+	# loop over all sources directories
+	for dir in "${sources_dirs[@]}"; do
+		# get just the basenames of all directories and symlinks and remove the first line ($sources_dir)
+		repos+=($(find "$dir" -maxdepth 1 \( -type d -o -type l \) -exec basename {} \; | sed '1d'))
+	done
+
 	_describe 'repositories' repos || compadd "${repos[@]}"
 }
 
