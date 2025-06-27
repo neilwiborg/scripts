c2041f639587144e0cfc8200c82d0bc93d1dabb5
Neil Wiborg <NeilWiborg@outlook.com>
2025-06-26 20:45:49 -0700

    add comments

--- repo.zsh
+++ repo.zsh
@@ -3,14 +3,18 @@
 typeset -g sources_dir="$HOME/cs"
 
 repo() {
-	if [ $# -eq 0 ]; then
+	# if no args or too many args passed
+	if [ $# -ne 1 ]; then
 		echo "Usage: repo <repository name>"
 		return 1
 	fi
 
+	# convert query into lowercase
 	local search_term=$(echo "$1" | tr '[:upper:]' '[:lower:]')
+	# search for directories and symlinks only one level deep in $sources_dir, and search case-insensitively
 	local target=$(find "$sources_dir" -maxdepth 1 \( -type d -o -type l \) -iname "*$search_term*" | head -n 1)
 
+	# if target is not empty
 	if [ -n "$target" ]; then
 		cd "$target" || return 1
 	else
@@ -21,6 +25,7 @@ repo() {
 
 # Tab completion for repo function
 _repo_complete() {
+	# get just the basenames of all directories and symlinks and remove the first line ($sources_dir)
 	local repos=($(find "$sources_dir" -maxdepth 1 \( -type d -o -type l \) -exec basename {} \; | sed '1d'))
 	_describe 'repositories' repos || compadd "${repos[@]}"
 }
