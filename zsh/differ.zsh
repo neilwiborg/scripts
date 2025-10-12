eba123be644c63725be14337e247ea666d0a954e
Neil Wiborg <NeilWiborg@outlook.com>
2025-10-11 17:08:34 -0700

    add swap menu option

--- zsh/differ.zsh
+++ zsh/differ.zsh
@@ -4,36 +4,43 @@ menu() {
     printf '\nDiffer menu:\n'
     printf '  1) Edit file 1\n'
     printf '  2) Edit file 2\n'
-    printf '  3) View diff (simple)\n'
-    printf '  4) View diff (visual)\n'
-    printf '  5) Exit\n'
-    printf 'Choose an option [1-5]: '
+    printf '  3) Swap files\n'
+    printf '  4) View diff (simple)\n'
+    printf '  5) View diff (visual)\n'
+    printf '  6) Exit\n'
+    printf 'Choose an option [1-6]: '
 }
 
 differ() {
-    local tmp1 tmp2 choice
+    local file1 file2 choice
 
-    tmp1=$(mktemp)
-    tmp2=$(mktemp)
+    file1=$(mktemp)
+    file2=$(mktemp)
 
     while true; do
         menu
         read -r choice
         case $choice in
             1)
-                $EDITOR "$tmp1"
+                $EDITOR "$file1"
                 ;;
             2)
-                $EDITOR "$tmp2"
+                $EDITOR "$file2"
                 ;;
             3)
-                git diff --no-index -- "$tmp1" "$tmp2"
+                local tmp
+                tmp=$file1
+                file1=$file2
+                file2=$tmp
                 ;;
             4)
-                git difftool --no-index -- "$tmp1" "$tmp2"
+                git diff --no-index -- "$file1" "$file2"
                 ;;
             5)
-                rm -f -- "$tmp1" "$tmp2"
+                git difftool --no-index -- "$file1" "$file2"
+                ;;
+            6)
+                rm -f -- "$file1" "$file2"
                 return 0
                 ;;
             *)
